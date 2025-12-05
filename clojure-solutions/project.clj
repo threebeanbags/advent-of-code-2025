83ed6c0f819364169820d55a7c7548797f48561e
threebeanbags <244434555+threebeanbags@users.noreply.github.com>
2025-12-04 16:05:51 -0800

    malli

--- clojure-solutions/project.clj
+++ clojure-solutions/project.clj
@@ -3,5 +3,6 @@
   :url "https://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.12.2"]]
+  :dependencies [[org.clojure/clojure "1.12.2"]
+                 [metosin/malli "0.20.0"]]
   :repl-options {:init-ns clojure-solutions.core})
